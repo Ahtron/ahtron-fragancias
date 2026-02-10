90cec9122ee4635b8311e700589a988ebc97d4c5
Ahtron Fragancias <capozzucogiovanni52@gmail.com>
2026-02-10 19:04:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,83 +14,98 @@ const perfumes = [
     {
         nombre: "9PM Elixir EDP 100ml",
         marca: "Afnan",
-        imagen: "img/perfumes/afnan_9pm_elixir.jpg", 
+        imagen: "img/perfumes/afnan_9pm_elixir.jpg",
+        link: "https://www.fragrantica.es/perfume/Afnan/9PM-Elixir-111894.html"
     },
     {
         nombre: "9PM EDP Masculino 100ml",
         marca: "Afnan",
-        imagen: "img/perfumes/afnan_9pm.jpg"
+        imagen: "img/perfumes/afnan_9pm.jpg",
+        link: "https://www.fragrantica.es/perfume/Afnan/9pm-65414.html"
     },
 
 // ARMAF
 {
     nombre: "Club de Nuit Intense Man EDT 105ml",
     marca: "Armaf",
-    imagen: "img/perfumes/armaf_cdn_intense.jpg"
+    imagen: "img/perfumes/armaf_cdn_intense.jpg",
+    link: "https://www.fragrantica.es/perfume/Armaf/Club-de-Nuit-Intense-Man-34696.html"
 },
 {
     nombre: "Club de Nuit Precieux 1 EDP 55ml",
     marca: "Armaf",
-    imagen: "img/perfumes/armaf_precieux.jpg"
+    imagen: "img/perfumes/armaf_precieux.jpg",
+    link: "https://www.fragrantica.es/perfume/Armaf/Club-de-Nuit-Precieux-I-93272.html"
 },
 {
     nombre: "Club de Nuit Maleka EDP 105ml",
     marca: "Armaf",
-    imagen: "img/perfumes/armaf_maleka.jpg"
+    imagen: "img/perfumes/armaf_maleka.jpg",
+    link: "https://www.fragrantica.es/perfume/Armaf/Club-De-Nuit-Maleka-106168.html"
 },
 {
     nombre: "Odyssey Mandarin Sky EDP 100ml",
     marca: "Armaf",
-    imagen: "img/perfumes/armaf_odyssey_mandarin_sky.jpg"
+    imagen: "img/perfumes/armaf_odyssey_mandarin_sky.jpg",
+    link: "https://www.fragrantica.es/perfume/Armaf/Odyssey-Mandarin-Sky-83132.html"
 },
 
     // LATTAFA
     {
         nombre: "Art of Universe EDP 100ml",
         marca: "Lattafa",
-        imagen: "img/perfumes/lattafa_art_of_universe.jpg"
+        imagen: "img/perfumes/lattafa_art_of_universe.jpg",
+        link: "https://www.fragrantica.es/perfume/Lattafa-Perfumes/Art-Of-Universe-101314.html"
     },
     {
         nombre: "Vintage Radio EDP 100ml",
         marca: "Lattafa",
-        imagen: "img/perfumes/lattafa_vintage_radio.jpg"
+        imagen: "img/perfumes/lattafa_vintage_radio.jpg",
+        link: "https://www.fragrantica.es/perfume/Lattafa-Perfumes/Vintage-Radio-89454.html"
     },
     {
         nombre: "Khamrah Qahwa EDP 100ml",
         marca: "Lattafa",
-        imagen: "img/perfumes/lattafa_khamrah_qahwa.jpg"
+        imagen: "img/perfumes/lattafa_khamrah_qahwa.jpg",
+        link: "https://www.fragrantica.es/perfume/Lattafa-Perfumes/Khamrah-Qahwa-88175.html"
     },
     {
         nombre: "Asad EDP 100ml",
         marca: "Lattafa",
-        imagen: "img/perfumes/lattafa_asad.jpg"
+        imagen: "img/perfumes/lattafa_asad.jpg",
+        link: "https://www.fragrantica.es/perfume/Lattafa-Perfumes/Asad-72821.html"
     },
     {
         nombre: "Asad Bourbon EDP 100ml",
         marca: "Lattafa",
-        imagen: "img/perfumes/lattafa_asad_bourbon.jpg"
+        imagen: "img/perfumes/lattafa_asad_bourbon.jpg",
+        link: "https://www.fragrantica.es/perfume/Lattafa-Perfumes/Asad-Bourbon-101124.html"
     },
     {
         nombre: "Badee Al Oud Honor & Glory EDP 100ml",
         marca: "Lattafa",
-        imagen: "img/perfumes/lattafa_honor_glory.jpg"
+        imagen: "img/perfumes/lattafa_honor_glory.jpg",
+        link: "https://www.fragrantica.es/perfume/Lattafa-Perfumes/Bade-e-Al-Oud-Honor-Glory-84302.html"
     },
     {
         nombre: "Badee Al Oud For Glory EDP 100ml",
         marca: "Lattafa",
-        imagen: "img/perfumes/lattafa_for_glory.jpg"
+        imagen: "img/perfumes/lattafa_for_glory.jpg",
+        link: "https://www.fragrantica.es/perfume/Lattafa-Perfumes/Bade-e-Al-Oud-Oud-for-Glory-64948.html"
     },
 
     // MAISON ALHAMBRA
     {
         nombre: "Jean Lowe Inmortel EDP 100ml",
         marca: "Maison Alhambra",
-        imagen: "img/perfumes/maison_jean_lowe_inmortel.jpg"
+        imagen: "img/perfumes/maison_jean_lowe_inmortel.jpg",
+        link: "https://www.fragrantica.es/perfume/Maison-Alhambra/Jean-Lowe-Immortal-83666.html"
     },
     {
         nombre: "Salvo Intense EDP 100ml",
         marca: "Maison Alhambra",
-        imagen: "img/perfumes/maison_salvo_intense.jpg"
+        imagen: "img/perfumes/maison_salvo_intense.jpg",
+        link: "https://www.fragrantica.es/perfume/Maison-Alhambra/Salvo-Intense-96001.html"
     }
 ];
 
@@ -133,6 +148,7 @@ for (let i = 0; i < botones.length; i++) {
 
 // Mostrar todo al cargar
 mostrarPerfumes();
+
 
 
 
