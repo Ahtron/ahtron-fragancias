a8dcd7e9bf41e9c17ef2cdffa1c79679d348183f
Ahtron Fragancias <capozzucogiovanni52@gmail.com>
2026-03-04 09:54:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,6 @@ const perfumes = [
         nombre: "Club de Nuit Intense Man EDT 105ml",
         marca: "Armaf",
         price: "Sin Stock",
-        price: "5ml" 8000,
         imagen: "img/perfumes/armaf_cdn_intense.jpg",
         link: "https://www.fragrantica.es/perfume/Armaf/Club-de-Nuit-Intense-Man-34696.html"
         
@@ -159,6 +158,7 @@ document.querySelectorAll(".filtros button").forEach(btn => {
 });
 
 mostrarPerfumes();
+
 
 
 
